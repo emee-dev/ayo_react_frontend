a5c24e2bd1ce31566ca0b9f5d554de366293b0c1
Emmanuel Ajike <emmanuelajike2000@gmail.com>
2023-06-21 11:28:44 +0100

    trying to convert jsx to js vite

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
-	esbuild: {
-		loader: "jsx",
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: {
-				".js": "jsx",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    // Set MIME type for JavaScript files
+    mimeTypes: {
+      ".js": "application/javascript",
+    },
+  },
+  esbuild: {
+    loader: "jsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
 });
+
